ac0347475a2d72b3813f14549def6e6b42d3d661
luguoliang <1158429526@qq.com>
2020-07-15 01:42:52 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-lgLib_Example/Pods-lgLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lgLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_lgLib_Example
+@end
---
+++ Example/Pods/Target Support Files/lgLib/lgLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_lgLib : NSObject
+@end
+@implementation PodsDummy_lgLib
+@end
